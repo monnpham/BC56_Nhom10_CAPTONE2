a4d723ed4dbaf8a8f489f2cc8272328a3aaad672
Mon <monnpham19@gmail.com>
2023-08-22 07:01:50 +0700

    push again

---
+++ asset/js/admin.js
@@ -0,0 +1,25 @@
+var button = document.createElement("button");
+button.innerHTML = "Add Product";
+button.classList.add("btn", "btn-add-product");
+
+var table = document.querySelector("table");
+var row = table.rows[0];
+
+row.appendChild(button);
+function openModal(modalId) {
+    // Get the modal
+    var modal = document.getElementById(modalId);
+
+    // Show the modal
+    modal.style.display = "block";
+}
+
+function closeModal(modalId) {
+    // Get the modal
+    var modal = document.getElementById(modalId);
+
+    // Hide the modal
+    modal.style.display = "none";
+}
+
+
